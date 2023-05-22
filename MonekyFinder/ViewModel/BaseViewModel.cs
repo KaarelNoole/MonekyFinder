bf2b4157ba21fe1ebca86da6f9ee9a8fd5b61389
KaarelNoole <kaarelnoole@gmail.com>
2023-05-22 13:17:35 +0300

    .NET Community Toolkit for MVVM (Source Generators)

--- MonekyFinder/ViewModel/BaseViewModel.cs
+++ MonekyFinder/ViewModel/BaseViewModel.cs
@@ -1,42 +1,21 @@
 ﻿namespace ViewModel;
-public class BaseViewModel : INotifyPropertyChanged
+
+
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+        
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(isNotBusy))]
     bool isBusy;
+
+    [ObservableProperty]
     string title;
-
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value)
-                return;
-
-            isBusy = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(isNotBusy));
-        }
-    }
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value)
-                return;
-            title = value;
-            OnPropertyChanged();
-        }
-    }
 
     public bool isNotBusy => !isBusy;
 
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public void OnPropertyChanged([CallerMemberName]string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
 }
 
