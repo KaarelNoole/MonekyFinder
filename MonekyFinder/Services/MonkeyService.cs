89bbe1b767067c296d42564f54dba983bb9cf74f
KaarelNoole <kaarelnoole@gmail.com>
2023-05-22 13:49:14 +0300

    HTTP REST Calls & JSON Deserialization

--- MonekyFinder/Services/MonkeyService.cs
+++ MonekyFinder/Services/MonkeyService.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Json;
 using System.Text;
 
 namespace Services
 {
-    internal class MonkeyService
+    public class MonkeyService
     {
+        HttpClient httpClient;
+        public MonkeyService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        List<Monkey> monkeyList = new ();
+        public async Task<List<Monkey>> GetMonkeys()
+        {
+            if (monkeyList?.Count > 0)
+                return monkeyList;
+
+            var url = "https://montemagno.com/monkeys.json";
+
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+            }
+
+            /*using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+            using var reader = new StreamReader(stream);
+            var contents = await reader.ReadToEndAsync();
+            monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);*/
+
+            return monkeyList;
+        }
     }
 }
--- MonekyFinder/ViewModel/MonkeysViewModel.cs
+++ MonekyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,7 +1,16 @@
-﻿
+﻿using Services;
 
 namespace ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+
 }
